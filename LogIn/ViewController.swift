771cda28d6d880fcd381297c7f871b932fbb6467
NatalyaLuzyanina <lnp3395@gmail.com>
2021-03-31 10:47:10 +0300

    Interface builded

--- LogIn/ViewController.swift
+++ LogIn/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showName() {
+        remind(credentials: "Your name is User", for: userName)
+    }
+   
+    @IBAction func showPassword() {
+        remind(credentials: "Your password is 123", for: password)
+    }
+    
+    private func remind(credentials: String?,
+                        for textField: UITextField!) {
+        
+        let messege = UIAlertController(
+            title: "Oops",
+            message: credentials,
+            preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(
+            title: "OK",
+            style: .default) {
+            _ in textField.text = ""
+        }
+        
+        messege.addAction(okAction)
+        present(messege, animated: true)
+    }
 }
 
